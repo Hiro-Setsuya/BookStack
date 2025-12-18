978eac34b6a9988779f09ac6b88da2bc4ad409ee
Hiro Setsuya <adrianvincenthjavillo@iskolarngbayan.pup.edu.ph>
2025-12-18 13:56:59 +0800

    Credentials of sms integration

---
+++ config/sms.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'gateway_url' => 'http://192.168.18.39:8080/messages', // full endpoint
+    'username'    => 'sms', // SMS gateway username
+    'password'    => 'lickmyez', // SMS gateway password
+];
