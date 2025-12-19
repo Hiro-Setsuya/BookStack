1d5b4af17caaa94c43ff1fba1fb12046cd54fd60
Rowel Gabriel <rowelgabrielvmangabat@gmail.com>
2025-12-19 19:36:45 +0800

    cart added

---
+++ cart.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap Demo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- navbar -->
+  <nav id="navbar" class="navbar navbar-expand-lg shadow-sm fixed-top px-sm-4 px-1 py-2 bg-light">
+    <div class="container-fluid">
+      <div class="navbar-brand fw-bold text-success">
+        BookStack
+      </div>
+      <div class="ms-auto">
+        <button class="btn btn-success d-lg-none d-inline-block me-2">Login/Register</button>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item px-lg-3">
+            <a class="nav-link fw-semibold" href="index.php">
+              <i class="bi bi-house-door-fill me-2"></i>Home
+            </a>
+          </li>
+          <li class="nav-item px-lg-3">
+            <a class="nav-link fw-semibold" href="ebooks.php">
+              <i class="bi bi-book-fill me-2"></i>E-Books
+            </a>
+          </li>
+          <li class="nav-item px-lg-3">
+            <a class="nav-link fw-semibold" href="ebook-details.php">
+              <i class="bi bi-grid-fill me-2"></i>Categories
+            </a>
+          </li>
+          <li class="nav-item px-lg-3 d-inline-block d-lg-none">
+            <a class="nav-link fw-semibold" href="cart.php">
+              <i class="bi bi-cart3-fill me-2"></i>Cart
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="ms-auto d-none d-lg-flex">
+        <input class="form-control me-2" type="text" placeholder="Search">
+        <button class="btn btn-success me-2"><i class="bi bi-cart3"></i></button>
+        <button class="btn btn-success">Login/Register</button>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col text-center mx-2">
+        <div class="mx-auto d-lg-none d-flex mt-4">
+          <input class="form-control me-2" type="text" placeholder="Search">
+          <button class="btn btn-success"><i class="bi bi-search"></i></button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-3 mt-lg-5 px-3">
+    <div class="row">
+
+      <div class="col-lg-8 mb-3">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+          <div class="h2 fw-bold">Your Cart</div>
+          <div class="fs-6">2 Items</div>
+        </div>
+
+        <div class="card p-3 mb-3">
+          <div class="row">
+            <div class="col-3">
+              <img src="https://m.media-amazon.com/images/I/519LoIgUAFL.jpg" height="120">
+            </div>
+            <div class="col-6">
+              <div class="fs-4 fw-bold">Sample Book 1</div>
+              <div class="mb-4">Sample Author 1</div>
+              <div class="btn btn-sm btn-danger me-2">
+                <i class="bi bi-trash-fill"></i>
+                <span class="d-none d-md-inline ms-2">Remove</span>
+              </div>
+              <div class="btn btn-sm btn-success">
+                <i class="bi bi-heart-fill"></i>
+                <span class="d-none d-md-inline ms-2">Save for Later</span>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="fs-5 fw-semibold">₱100.00</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="card p-3 mb-2">
+          <div class="row">
+            <div class="col-3">
+              <img src="https://imgv2-2-f.scribdassets.com/img/word_document/317276308/original/adfe3e30f6/1585165423?v=1" height="120">
+            </div>
+            <div class="col-6">
+              <div class="fs-4 fw-bold">Sample Book 2</div>
+              <div class="mb-4">Sample Author 2</div>
+              <div class="btn btn-sm btn-danger me-2">
+                <i class="bi bi-trash-fill"></i>
+                <span class="d-none d-md-inline ms-2">Remove</span>
+              </div>
+              <div class="btn btn-sm btn-success">
+                <i class="bi bi-heart-fill"></i>
+                <span class="d-none d-md-inline ms-2">Save for Later</span>
+              </div>
+            </div>
+            <div class="col-3">
+              <div class="fs-5 fw-semibold">₱100.00</div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      <div class="col-lg-4 mb-3">
+        <div class="card p-3 position-sticky" style="top: 90px;">
+          <div class="card-title fs-4 fw-bold mb-3">Order Summary</div>
+          <div class="d-flex justify-content-between align-items-center mb-3">
+            <div class="fs-6 fw-semibold">Subtotal</div>
+            <div class="fs-6">₱100.00</div>
+          </div>
+          <div class="d-flex justify-content-between align-items-center mb-3 text-success">
+            <div class="fs-6 fw-semibold">Discount (30%)</div>
+            <div class="fs-6">₱30.00</div>
+          </div>
+          <div class="d-flex justify-content-between align-items-center mb-3">
+            <div class="fs-6 fw-semibold">Tax (Estimated)</div>
+            <div class="fs-6">₱0.00</div>
+          </div>
+          <div class="d-flex justify-content-between align-items-center border-top border-2 mb-3">
+            <div class="fs-5 fw-bold">Total</div>
+            <div class="fs-5 fw-semibold">₱70.00</div>
+          </div>
+          <div class="fs-6 d-block ms-2">Promo Code</div>
+          <div class="d-flex mb-5">
+            <input class="form-control me-2" type="text" placeholder="Enter Code">
+            <button class="btn btn-sm btn-success">Apply</button>
+          </div>
+          <div class="btn btn-success">Proceed to Checkout</div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
