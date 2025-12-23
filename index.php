2f84f405a4bd8129f4b67b585d6c6632c2479571
KeoTech <chinokeonie.santos@gmail.com>
2025-12-23 23:28:15 +0800

    Integrate chatbot

--- index.php
+++ index.php
@@ -114,8 +114,8 @@ session_start();
       </div>
     </div>
   </div>
-
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  <?php include 'chatbot/chatbot.php'; ?>
 </body>
 
 </html>
