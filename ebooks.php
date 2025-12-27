fdd44cb227f2138d49c9eab0a54143f31335eb6b
KeoTech <chinokeonie.santos@gmail.com>
2025-12-28 01:34:22 +0800

    Included chatbot.php at the end

--- ebooks.php
+++ ebooks.php
@@ -214,6 +214,7 @@ $result = executeQuery($query);
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+      <?php include 'chatbot/chatbot.php'; ?>
 </body>
 
 </html>
