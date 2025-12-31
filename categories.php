1068fc23c1c3f17d87863bbf500264552cbc3876
Hiro Setsuya <adrianvincenthjavillo@iskolarngbayan.pup.edu.ph>
2025-12-31 19:04:52 +0800

    Add additional page

---
+++ categories.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BookStack - Computer & Tech E-Books</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <!-- navbar -->
+    <?php include 'includes/nav.php'; ?>
+
+    <h1>
+        Categories Page
+    </h1>
+    <?php include 'includes/footer.php'; ?>
+</body>
+
+</html>
