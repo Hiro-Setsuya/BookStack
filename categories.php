a1a1d77c7c9dcf0ff0f049d48705e5b1ab763d31
KeoTech <chinokeonie.santos@gmail.com>
2026-01-09 15:08:56 +0800

    Add chatbot to categories page

--- categories.php
+++ categories.php
@@ -80,7 +80,7 @@ include 'includes/head.php';
     </div>
 
     <?php include 'includes/footer.php'; ?>
-
+    <?php include 'includes/footer.php'; ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
