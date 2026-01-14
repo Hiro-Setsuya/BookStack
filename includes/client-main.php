c9437bdc00142fe6ad96c442ad58390f35553769
Rowel Gabriel <rowelgabrielvmangabat@gmail.com>
2026-01-14 13:32:19 +0800

    search button alignment fixed

--- includes/client-main.php
+++ includes/client-main.php
@@ -6,12 +6,13 @@
         </div>
 
         <div class="col-md-5"> <?php if (basename($_SERVER['PHP_SELF']) === 'ebooks.php'): ?>
-                <form method="GET" action="ebooks.php" class="d-flex">
-                    <div class="input-group">
-                        <input class="form-control" type="text" name="q"
-                            placeholder="Search books, authors..."
+                <form method="GET" action="ebooks.php" class="row g-2 align-items-stretch">
+                    <div class="col-10">
+                        <input class="form-control h-100" type="text" name="q" placeholder="Search books, authors..."
                             value="<?php echo isset($_GET['q']) ? htmlspecialchars($_GET['q']) : ''; ?>" />
-                        <button class="btn btn-green" type="submit">
+                    </div>
+                    <div class="col-2">
+                        <button class="btn btn-green w-100 h-100" type="submit">
                             <i class="bi bi-search"></i>
                         </button>
                     </div>
